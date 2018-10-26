6642c8a584906ecb66c98b2fb046be10def9a91c
Rajesh <rajesh.kumar@tammina.com>
2018-10-26 12:58:08 +0530

    code changed as suggested

--- test/go-ducks-test.js
+++ test/go-ducks-test.js
@@ -1,16 +1,17 @@
 const assert = require('assert'),
-    vows = require('vows'),
-    { goDucks } = require('../go-ducks');
+  vows = require('vows'),
+  { goDucks } = require('../go-ducks');
 
 vows.describe('serious-calculations').addBatch({
   'when performing serious calculations': {
     topic: () => {
-      goDucks.appendMessages([()=>'five by five']);
+      // goDucks.appendMessages([()=>'five by five']);
       return goDucks.quack();
     },
     'result should be valid': (topic) => {
       assert.isString(topic);
-      assert.equal(topic, 'five');
+      // assert.equal(topic, 'five');
+      assert.equal(topic[0], `IT'S A SECRET TO EVERYBODY`);
     }
   }
 }).export(module);
