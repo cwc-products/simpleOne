eef19fcf8f96f0769a3f156e16a18b095010345f
tejasakala <teja.sakala@tammina.com>
2018-10-26 16:57:05 +0530

    ITS resolved conflicts

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
 const { goDucks } = require('./go-ducks');
-<<<<<<< HEAD
 goDucks.appendMessages([() => "IT'S"]);
 console.log(goDucks.quack());
-=======
-console.log(goDucks.quack());
->>>>>>> staging
